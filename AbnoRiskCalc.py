221ad56e101689fb10114a6fca002c31c3312e95
IchoTM <jms01192006@gmail.com>
2023-09-26 17:54:44 -0400

    Working version of the abno calc before errors

--- AbnoRiskCalc.py
+++ AbnoRiskCalc.py
@@ -12,18 +12,49 @@ def riskClassCalc(dmg,diff,bp):
     else:
         print("something went wrong")
 def getBreachPotential():
-    bp = int(input("How likely is it for the Abno to breach? 1) Not Likely 2) Possible 3) Likely 4) Very Likely: "))
+    print("How likely is it for the Abnormality to breach its containment?")
+    print("1) Not Likely")
+    print("2) Possible")
+    print("3) Likely")
+    print("4) Very Likely")
+    bp = int(input(">"))
     return bp
 def getDmgReach():
-    dmg = int(input("In the event of a breach will the Abno's damage reach? 1) Single Target 2) Room Wide 3) Deparment Wide 4) Facility Wide: "))
+    print("In the event of a breach will the Abnormality's damage reach?")
+    print("1) Single Target")
+    print("2) Room Wide")
+    print("3) Deparment Wide")
+    print("4) Facility Wide")
+    dmg = int(input(">"))
     return dmg
 def workDiffTotal():
-    diff = int(input("On a scale of 1 to 10 what is the work success rate of this Abno?: "))
+    diff = int(input("On a scale of 1 to 10 what is the work success rate of this Abnormality?: "))
     return diff
+def varCheck(w,g,b):
+    error = int()
+    error = 0
+    if w not in range(1,11):
+        error = 1
+        print("W fail")
+        return error
+    elif g not in range(1,5):
+        error = 1
+        print("G fail")
+        return error
+    elif b not in range(1,5):
+        error = 1
+        print("B fail")
+        return error
+    else:
+        return error
 def main():
     diff = workDiffTotal()
     dmg = getDmgReach()
     bp = getBreachPotential()
-    risk = riskClassCalc(dmg,diff,bp)
+    vc = varCheck(diff,dmg,bp)
+    if vc == 1:
+        print("Something is wrong")
+    else:
+        risk = riskClassCalc(dmg,diff,bp)
 
 main()
